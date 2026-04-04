a0087794fb3ee6173dba260020a0b38a35154f45
maratedorescu11 <m.teodorescu@student.utwente.nl>
2026-04-04 12:34:48 +0200

    feat: add SearchBar component with input and search emit

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const emit = defineEmits<{
+  search: [postcode: string]
+}>()
+
+const postcode = ref('')
+
+function handleSubmit() {
+  emit('search', postcode.value)
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input
+      v-model="postcode"
+      type="text"
+      placeholder="Enter a UK postcode"
+    />
+    <button type="submit">Search</button>
+  </form>
+</template>
