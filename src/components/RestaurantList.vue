ee58ec280a4e68da67e875d26bd8c47418410375
maratedorescu11 <m.teodorescu@student.utwente.nl>
2026-04-05 14:19:56 +0200

    feat: add RestaurantList component rendering a card per restaurant

---
+++ src/components/RestaurantList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Restaurant } from '../types/restaurant'
+import RestaurantCard from './RestaurantCard.vue'
+
+defineProps<{
+  restaurants: Restaurant[]
+}>()
+</script>
+
+<template>
+  <div>
+    <!-- render one card per restaurant -->
+    <RestaurantCard
+      v-for="restaurant in restaurants"
+      :key="restaurant.id"
+      :restaurant="restaurant"
+    />
+  </div>
+</template>
