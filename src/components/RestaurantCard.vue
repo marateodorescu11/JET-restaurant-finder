beb1a2f63f1b5a994e95e0bbfe85550f3409e4f1
maratedorescu11 <m.teodorescu@student.utwente.nl>
2026-04-05 13:58:39 +0200

    feat: add basic structure for RestaurantCard component

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Restaurant } from '../types/restaurant'
+
+const props = defineProps<{//what data is expected to receive
+  restaurant: Restaurant
+}>()
+</script>
+
+<template>
+  <div>
+    <h2>{{ props.restaurant.name }}</h2>//the restaurant name is displayed as a heading
+    <p>{{ props.restaurant.cuisines }}</p>
+    <p>{{ props.restaurant.rating.starRating }}</p>
+    <p>{{ props.restaurant.address }}</p>
+  </div>
+</template>
